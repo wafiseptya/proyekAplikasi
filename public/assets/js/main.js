9836102567cfd4f6d574fc2cbd6c79ff5e788197
Wafi <septya.wafi@gmail.com>
2018-11-04 11:19:24 +0700

    landing page

---
+++ public/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   // nicescroll
+	 $("html").niceScroll({
+		 cursorcolor:"#95a5a6",
+		 cursorwidth:"5px",
+		 cursorborder:"1px solid transparent",
+		 cursorborderradius:"0px"
+	 });
+
+ });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/base', function () {
+    return view('template/base');
+});
