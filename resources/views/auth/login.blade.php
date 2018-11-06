3a384ebde43f5a108a90f955e73df1df6f4dee75
Wafi <septya.wafi@gmail.com>
2018-11-07 01:32:36 +0700

    add branch wafi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('template/base')
 
 @section('content')
-<div class="container mt-5 pt-5">
+<div class="container mt-5 pt-3">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
